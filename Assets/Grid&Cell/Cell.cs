550801629670b5bedf46cfc6bbd7a4cc6f146888
RavenChelm <98704006+RavenChelm@users.noreply.github.com>
2022-06-28 12:02:50 +0300

    Add step_BackLight System
    Через соседей клетки передаётся сообщение о том, чтобы клетка поменяла цвет.
    Сейчас нужно добавить отметки о посещение клетки

--- Assets/Grid&Cell/Cell.cs
+++ Assets/Grid&Cell/Cell.cs
@@ -4,41 +4,87 @@ using UnityEngine;
 
 public class Cell : MonoBehaviour
 {
-    [SerializeField] private Material defaultColor; // сейчас не нужно
-    [SerializeField] private Material changeColor;
-    [SerializeField] public MeshRenderer _meshRenderer;
-    [SerializeField] public GameObject player; //нужно ли SerializeField?
+    [SerializeField] private Material defaultColor; // обычный цвет
+    [SerializeField] private Material hoverColor;   // цвет при наведение мыши
+    [SerializeField] private Material backColor;    // подсветка при выборе движения
+    [SerializeField] public MeshRenderer _meshRenderer; //Нужно ли? 
     private PlayerController _playerController;
     public List<Cell> neighbours;
-
+    private int step = -1;
+    public bool mark = false;
+    private int h = 0;
     private void Start()
     {
-        player = GameObject.FindWithTag("Player");              //Не работает Ссылка на объект, при добоваление через инспектор
-        _playerController = player.GetComponent<PlayerController>(); //Возможно изменить, если будет провисать оптимизация, слишком много объектов делают find, но хорошо, что один раз.
-        _meshRenderer.enabled = false;
+        _playerController = GameObject.FindWithTag("Player").GetComponent<PlayerController>(); //Возможно изменить, если будет провисать оптимизация, слишком много объектов делают find, но хорошо, что один раз.
+        ChangeColor(-1);
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Cell")
         { neighbours.Add(other.gameObject.GetComponent<Cell>()); }
     }
-
     private void OnMouseEnter()
     {
-        _meshRenderer.enabled = true;   //Изначально клетки не видны
-
-        _meshRenderer.material = changeColor;   //Подсвечиваются при наведение мышки
-
+        ChangeColor(0);//Подсвечиваются при наведение мышки
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[0].transform.position));
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[1].transform.position));
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[2].transform.position));
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[3].transform.position));
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[4].transform.position));
+        // Debug.Log(Vector3.Distance(this.transform.position, neighbours[5].transform.position));
     }
     private void OnMouseExit()
     {
-        _meshRenderer.enabled = false;
-        _meshRenderer.material = defaultColor;
+        if (_meshRenderer.material != backColor)
+        {
+            ChangeColor(-1);
+        }
+
     }
     private void OnMouseDown()
     {
         if (!_playerController.inWay)
             _playerController.targetCell = this;
     }
+
+
+    private void ChangeColor(int color)
+    {
+        switch (color)
+        {
+            case 0: //при наведение
+                _meshRenderer.enabled = true;
+                _meshRenderer.material = hoverColor;
+                break;
+            case 1: //подсветка доступных для перемещения
+                _meshRenderer.enabled = true;
+                _meshRenderer.material = backColor;
+                break;
+            case -1: //изначально выключенны
+                _meshRenderer.enabled = false;
+                break;
+        }
+    }
+
+    public void StepBackLight(int lastStep)
+    {
+        step = lastStep;
+        mark = true;
+        while (step < _playerController.AvalibleDistance)
+        {
+            for (int i = 0; i < neighbours.Count; i++)
+            {
+                float distanceToNeigbours = Vector3.Distance(this.transform.position, neighbours[i].transform.position);
+                if (((distanceToNeigbours == (float)1 && neighbours[i].h == this.h) ||
+                    (distanceToNeigbours == (float)1.414214) && neighbours[i].h == this.h + 1) &&  //Изменить дистанцую карабканья
+                    !neighbours[i].mark)
+                {
+                    step++;
+                    neighbours[i].ChangeColor(1);
+                    //neighbours[i].StepBackLight(step);
+                }
+            }
+        }
+    }
 }
+
--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -8,13 +8,16 @@ public class PlayerController : MonoBehaviour
     private Transform transf;
     private NavMeshAgent m_Agent;
     public Cell nowCell;
+    public Cell lasCell;
     public Cell targetCell;
     public bool inWay = false;
+    public int AvalibleDistance;
     private void Start()
     {
         transf = gameObject.GetComponent<Transform>();
         nowCell = gameObject.GetComponent<Cell>();
         targetCell = gameObject.GetComponent<Cell>();
+        lasCell = gameObject.GetComponent<Cell>();
         _rigidBody = gameObject.GetComponent<Rigidbody>();
         m_Agent = gameObject.GetComponent<NavMeshAgent>();
         _rigidBody.WakeUp();
@@ -25,24 +28,32 @@ public class PlayerController : MonoBehaviour
     }
     private void MoveCharacter()
     {
+
         if (targetCell != null && !m_Agent.hasPath)
         {
             m_Agent.destination = targetCell.transform.position;
+            this.nowCell.StepBackLight(0);
         }
         else
         {
             targetCell = null;
         }
-        Debug.Log(m_Agent.hasPath);
+        //Debug.Log(m_Agent.hasPath);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Cell")
-            if (!this.inWay)
-            {
-                this.nowCell = other.gameObject.GetComponent<Cell>();
-            }
+        {
+            this.nowCell = other.gameObject.GetComponent<Cell>();
+        }
     }
-
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Cell")
+        {
+            this.lasCell = other.gameObject.GetComponent<Cell>();
+            lasCell.mark = false;
+        }
+    }
 }
